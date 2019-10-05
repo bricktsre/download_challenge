86d90915d7eed9f39d8821005c42112e5fca1a55
bricktsre <gwoodward21@gmail.com>
2019-10-05 18:00:55 -0400

    Parse command line arguments and options

--- downloader.go
+++ downloader.go
@@ -1,12 +1,28 @@
 package main
 
-import { 
+import ( 
 	"fmt"
-	"net/http"
-	"io/ioutil"
+	//"net/http"
+	//"io/ioutil"
 	"strconv"
-	"sync"
+	//"sync"
+	"os"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	if len(os.Args) < 4 || len(os.Args) > 5{
+		fmt.Println("Proper usage: ./downloader <url> -c numberOfThreads")
+		return	
+	}
+	url := os.Args[1]
+	
+	if os.Args[2] != "-c" {
+		fmt.Println("Proper usage: ./downloader <url> -c numberOfThreads")
+		return
+	}
+	numThreads, err := strconv.Atoi(os.Args[3])
+	if err != nil { 
+		panic(err)
+	}
+	fmt.Println(url, numThreads)
 }
